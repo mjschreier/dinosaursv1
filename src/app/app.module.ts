bf1fc0b58503106e28e48b4d2aed095d979fc0a2
Michael Schreier <mjschreier1@gmail.com>
2018-01-17 16:47:19 -0700

    dev environment set - gen components - copied files dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { JobListComponent } from './components/job-list/job-list.component';
+import { JobFormComponent } from './components/job-form/job-form.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    JobListComponent,
+    JobFormComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
