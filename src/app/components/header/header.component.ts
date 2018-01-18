20f6aa77396b70bc6c4468c553a059ce897ac4d1
Michael Schreier <mjschreier1@gmail.com>
2018-01-17 20:56:07 -0700

    matched margins in css & assembled placeholder <main>

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  imgSrc: string;
+  imgAlt: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.imgSrc = '../../../assets/g-dino.png'
+    this.imgAlt = 'g-dino logo'
   }
 
 }
